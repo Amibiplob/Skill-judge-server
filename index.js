9d4294042132585bacd5d592d1a6744aa26d0944
FreeDom-Coder-007 <afzalsharif623@gmail.com>
2023-02-13 10:50:36 +0600

    updated to ES6 module

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const cors = require("cors");
-const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
+import express, { json } from "express";
+import cors from "cors";
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 
 const app = express();
 require("dotenv").config();
 
 app.use(cors());
-app.use(express.json());
+app.use(json());
 const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
@@ -23,8 +23,8 @@ const client = new MongoClient(uri, {
 
 
 
-const blog = require("./blog");
-const compiler = require("./compiler");
+import blog from "./blog";
+import compiler from "./compiler";
 
 
 
